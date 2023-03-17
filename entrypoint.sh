2b9d2e012e86487de836afc10f0e7ce12481612b
Dickson Afful <fiifi.dev@gmail.com>
2023-03-16 21:03:02 -0400

    chore: reduce workers

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 
 python scripts/create_database.py
 alembic upgrade head
-gunicorn app.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:80
+gunicorn app.main:app --workers 2 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:80
